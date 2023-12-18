bd38e1ee0340d8823053fc3f607abcd8c80267df
David Kozdra <45106564+DavidKozdra@users.noreply.github.com>
2023-12-18 04:18:15 -0500

    Added generation logic to fix #29 (#41)
    * fixed the requirments by adding them first added logic for if the length is too low
    
    * fixed order , and insecure randomness
    
    * fixed not selected bug
    
    * fixed, does not add chars when there not defined

--- src/utils/generatePassword.ts
+++ src/utils/generatePassword.ts
@@ -5,7 +5,8 @@ import {
 import passwordStrength from "./passwordStrength";
 
 const generatePassword = (
-  attributes: PasswordAttributesType
+  attributes: PasswordAttributesType,
+  existingPasswords: string[] = []
 ): ProPasswordReturnType => {
   const charset = {
     upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
@@ -16,19 +17,6 @@ const generatePassword = (
 
   let password = "";
   let characters = "";
-
-  if (
-    !attributes.upper &&
-    !attributes.lower &&
-    !attributes.symbol &&
-    !attributes.number &&
-    attributes.salt.length > 0
-  ) {
-    return {
-      password: attributes.salt,
-      strength: passwordStrength(attributes.salt),
-    };
-  }
 
   if (
     !attributes.upper &&
@@ -47,19 +35,57 @@ const generatePassword = (
     };
   }
 
+  // Add existing passwords to the characters pool
+  existingPasswords.forEach((existingPassword) => {
+    characters += existingPassword;
+  });
+
   if (attributes.upper) characters += charset.upper;
   if (attributes.lower) characters += charset.lower;
   if (attributes.symbol) characters += charset.symbol;
   if (attributes.number) characters += charset.number;
 
-  let length = Array.isArray(attributes.length)
-    ? attributes.length[0]
-    : attributes.length;
+  const addCharacterOfType = (type: boolean, characters: string, maxLength: any) =>
+    type && password.length < maxLength
+      ? characters[Math.floor(getSecureRandom() * characters.length)]
+      : '';
 
-  for (let i = 0; i < length; i++) {
-    password += characters[Math.floor(Math.random() * characters.length)];
+  const getSecureRandom = () => {
+    const randomValues = new Uint32Array(1);
+    window.crypto.getRandomValues(randomValues);
+    return randomValues[0] / (0xffffffff + 1);
+  };
+
+  const types = [
+    { type: attributes.upper, characters: charset.upper },
+    { type: attributes.lower, characters: charset.lower },
+    { type: attributes.symbol, characters: charset.symbol },
+    { type: attributes.number, characters: charset.number },
+  ];
+
+  // Shuffle the order of types randomly
+  for (let i = types.length - 1; i > 0; i--) {
+    const j = Math.floor(getSecureRandom() * (i + 1));
+    [types[i], types[j]] = [types[j], types[i]];
   }
 
+  // Add characters of each type in the shuffled order
+  for (const { type, characters } of types) {
+    password += addCharacterOfType(type, characters, attributes.length);
+  }
+
+  // Generate the rest of the password based on the remaining length
+  let remainingLength = Array.isArray(attributes.length)
+    ? attributes.length[0] - password.length
+    : attributes.length - password.length;
+
+  if(characters ){
+    for (let i = 0; i < remainingLength; i++) {
+      password += characters[Math.floor(getSecureRandom() * characters.length)];
+    }
+  }
+
+  // Add salt if provided and position specified
   switch (attributes.saltAt) {
     case "s":
       password = attributes.salt + password;
@@ -78,6 +104,8 @@ const generatePassword = (
       break;
   }
 
+
+  // Calculate password strength
   const strength = passwordStrength(password);
 
   return {
